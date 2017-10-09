15001fd2ba0197c60a032cc5ba666548f1cdff5b
TM <tm@macadmin.ru>
2017-10-09 12:29:36 +0300

    create Skel

--- project.clj
+++ project.clj
@@ -3,4 +3,19 @@
   :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [mount "0.1.11"]
+                 [cprop "0.1.11"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [org.slf4j/slf4j-log4j12 "1.7.25"]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jmdk/jmxtools
+                                                    com.sun.jmx/jmxri]]
+                 [com.layerware/hugsql "0.4.7"]
+                 [org.clojure/java.jdbc "0.7.0"]
+                 [net.sourceforge.jtds/jtds "1.3.1"]]
+  :main pik-logistic-api.core
+  :profiles {:uberjar {:omit-source true
+                       :aot :all
+                       :uberjar-name "pik-logistic-api.jar"}})
--- src/pik_logistic_api/core.clj
+++ src/pik_logistic_api/core.clj
@@ -1,6 +1,28 @@
-(ns pik-logistic-api.core)
+(ns pik-logistic-api.core
+  (:require [clojure.tools.logging :as log]
+            [mount.core :as mount]
+            [pik-logistic-api.config :refer [settings]])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def state (atom {}))
+
+(defn init [args]
+  (log/info "PIK logistic API starting...")
+  (swap! state assoc :running true)
+  (mount/start #'settings))
+
+(defn stop []
+  (swap! state assoc :running false)
+  (log/info "Stopping...")
+  (shutdown-agents)
+  (Thread/sleep 1000)
+  (log/info "Stopped!"))
+
+(defn -main [& args]
+  (init args)
+  (log/info (identity settings))
+  (.addShutdownHook (Runtime/getRuntime)
+                    (Thread. stop))
+  (while (:running @state)
+    (log/info "tick")
+    (Thread/sleep 1000)))
