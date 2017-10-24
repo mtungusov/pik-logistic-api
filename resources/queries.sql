02d2b39d638d7655cadfa4be20f9036f27de4972
TM <tm@macadmin.ru>
2017-10-24 16:37:56 +0300

    change. change fields

--- resources/queries.sql
+++ resources/queries.sql
@@ -3,7 +3,8 @@ SELECT id, label,
   status_movement, status_connection,
   event, event_time,
   group_id, group_title,
-  zone_id, zone_parent_id, zone_label,
+  zone_id, zone_label,
+  zone_parent_id, zone_parent_label,
   zone_label_current, zone_label_prev, last_parent_inzone_time
 FROM trackers_info
 WHERE event_time > :time
@@ -11,4 +12,4 @@ ORDER BY (CASE
           WHEN event = 'inzone' OR (event = 'outzone' AND zone_parent_id IS NOT NULL) THEN 1
           WHEN event = 'outzone' AND zone_parent_id IS NULL THEN 2
           ELSE 3
-          END ), zone_label_current, event_time DESC
+          END ), zone_label_current, event_time
