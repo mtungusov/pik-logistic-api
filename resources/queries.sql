e24071009c1aa2a6bd8532b5386e2efe3c245cab
TM <tm@macadmin.ru>
2017-11-10 17:17:08 +0300

    change. query by gps-update-time

--- resources/queries.sql
+++ resources/queries.sql
@@ -1,5 +1,5 @@
 -- :name trackers-info :? :*
-SELECT id, label, 
+SELECT id, label,
   status_movement, status_connection,
   event, event_time, status_last_update, status_gps_update,
   group_id, group_title,
@@ -7,7 +7,7 @@ SELECT id, label,
   zone_parent_id, zone_parent_label,
   zone_label_current, zone_label_prev, last_parent_inzone_time
 FROM trackers_info
-WHERE event_time > :time
+WHERE status_gps_update > :time
 ORDER BY (CASE
           WHEN event = 'inzone' OR (event = 'outzone' AND zone_parent_id IS NOT NULL) THEN 1
           WHEN event = 'outzone' AND zone_parent_id IS NULL THEN 2
--- src/pik_logistic_api/db/queries.clj
+++ src/pik_logistic_api/db/queries.clj
@@ -7,6 +7,7 @@
 
 (def navyixy-time-formatter (tf/formatter "yyyy-MM-dd HH:mm:ss"))
 (def one-day (t/days 1))
+(def seven-days (t/days 7))
 
 (defn- to-sql-time [t]
   (tf/unparse navyixy-time-formatter t))
@@ -14,7 +15,8 @@
 ;(to-sql-time (t/minus (t/now) one-day))
 ;(-> (t/now) (t/minus one-day) to-sql-time)
 
+; time -> status_gps_update
 (defn get-trackers-info
   ([conn time] (trackers-info conn {:time time}))
-  ([conn] (let [time (-> (t/now) (t/minus one-day) to-sql-time)]
+  ([conn] (let [time (-> (t/now) (t/minus seven-days) to-sql-time)]
             (get-trackers-info conn time))))
